74f4e19aeb78cad27e78e00dbfb22e2076aecfc3
m-r-joyce <michaeljoyce@live.ca>
2017-05-28 21:48:54 -0400

    Encryption function added with ROT0 and ROT1 available

--- VigenèreCipher/Form1.cs
+++ VigenèreCipher/Form1.cs
@@ -12,10 +12,9 @@ namespace VigenèreCipher
 {
     public partial class Form1 : Form
     {
-        const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        int rotation;        
-        string function;
+        int rotation;      
 
         public Form1()
         {
@@ -31,6 +30,7 @@ namespace VigenèreCipher
             string key = KeyTextbox.Text;
             string input = InputTextbox.Text;
             string output;
+            string function = EncryptBtn.Checked ? "Encrypt" : "Decrypt";
 
             if (function == "Encrypt")
             {
@@ -40,6 +40,8 @@ namespace VigenèreCipher
             {
                 output = Decrypt(key, input, rotation);
             }
+
+            OutputTextbox.Text = output;
             
             watch.Stop();
             var elapsedTime = watch.ElapsedMilliseconds;
@@ -50,8 +52,36 @@ namespace VigenèreCipher
         {
             input = input.ToUpper();
             key = key.ToUpper();
+            int keyIndex = 0;
+            string encryptedText = "";
 
-            return "";
+            //For each letter in the input
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((char)input[i] == ' ')
+                {
+                    encryptedText += " ";
+                }
+                else
+                {
+                    int inputAlphaIndex = ALPHABET.IndexOf(input[i]);
+                    int keyAlphaIndex = ALPHABET.IndexOf(key[keyIndex]);
+                    char encryptedChar = ALPHABET[((inputAlphaIndex + keyAlphaIndex) % 26) + rotation];
+                    encryptedText += encryptedChar;
+                    keyIndex++;
+                }
+
+                if (keyIndex == key.Length)
+                {
+                    keyIndex = 0;
+                }
+            }
+            //Find the index of that letter in the alphabet.
+            //Then find the index of the current letter in the key.
+            //Add the two
+            //Find that index.
+
+            return encryptedText;
         }
 
         private string Decrypt(string key, string input, int rotation)
@@ -64,16 +94,6 @@ namespace VigenèreCipher
 
         private void FunctionBtn_CheckedChanged(object sender, EventArgs e)
         {
-            RadioButton rbSender = (RadioButton)sender;
-
-            if (rbSender.Text == "Encrypt")
-            {
-                function = "Encrypt";
-            }
-            else
-            {
-                function = "Decrypt";
-            }
         }
 
         private void RotationBtn_CheckChanged(object sender, EventArgs e)
